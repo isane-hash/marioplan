a7174790bc555f719b7a31f0643328ce00a14d08
isane-hash <isanejossy@gmail.com>
2020-02-03 09:22:20 +0200

    Updates

---
+++ precache-manifest.118f44c093ba652dbd4fe715d9e9996c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d75b07414842dbd2ce4",
+    "url": "/marioplan/static/css/main.1abae62e.chunk.css"
+  },
+  {
+    "revision": "6d75b07414842dbd2ce4",
+    "url": "/marioplan/static/js/main.6d75b074.chunk.js"
+  },
+  {
+    "revision": "af8f529953fe71781b61",
+    "url": "/marioplan/static/js/1.af8f5299.chunk.js"
+  },
+  {
+    "revision": "2d708f56a2c6dcc6802f",
+    "url": "/marioplan/static/js/runtime~main.2d708f56.js"
+  },
+  {
+    "revision": "4c579f3a25d7fa648752f5ecb9e5a9af",
+    "url": "/marioplan/index.html"
+  }
+];
